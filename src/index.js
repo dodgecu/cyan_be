0f56ae7ca71e14a505d07f86f0fd79d2cffbcdc5
AndriiSendun <andrii.sendun@gmail.com>
2019-04-26 22:27:54 +0300

    delete unnecessery files

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const cors = require('cors');
 
 const bodyParser = require('body-parser');
 
-const os = require('os');
-const networkInterfaces = os.networkInterfaces().en0[1].address;
-
 const flowerRoute = require('./routes/flower');
 const flowerSensors = require('./routes/flower-sensors');
 
@@ -34,6 +31,6 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () =>
   console.info(
-    `Server running on port ${PORT} locally available on ${networkInterfaces}`
+    `Server running on port ${PORT}`
   )
 );
