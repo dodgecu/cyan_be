da777b30c267be24fa1916a625a38e6d89155c65
Pavlo Pihula <rogertaylor1949@gmail.com>
2019-04-26 18:04:15 -0400

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const bodyParser = require('body-parser');
+const http = require('http').Server(app);
 
-const os = require('os');
-const networkInterfaces = os.networkInterfaces().en0[1].address;
+const PORT = process.env.PORT || 4000;
+
+const bodyParser = require('body-parser');
 
 const flowerRoute = require('./routes/flower');
 const flowerSensors = require('./routes/flower-sensors');
@@ -25,10 +26,4 @@ app.use(flowerRoute);
 
 app.use(flowerSensors);
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () =>
-  console.info(
-    `Server running on port ${PORT} locally available on ${networkInterfaces}`
-  )
-);
+app.listen(PORT, () => console.info(`Server running on port ${PORT}`));
--- src/sockets/sensors-data.js
+++ src/sockets/sensors-data.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const io = require('socket.io-client');
 const axios = require('axios');
+const socket_io = require('socket.io').listen(1335);
 
 const socket = io.connect('http://cyan:8080/', {
-  reconnection: true
+  reconnection: false
 });
 
 const sensorData = socket.on('connect', () => {
@@ -14,10 +15,17 @@ const sensorData = socket.on('connect', () => {
 });
 
 function postData(sensors) {
-  axios
-    .post('http://localhost:4000/flower-sensors', sensors)
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err));
+  console.log(sensors);
+  if (sensors === null) {
+    console.log('error');
+  } else {
+    axios
+      .post('http://localhost:4000/flower-sensors', sensors)
+      .then(res => {
+        socket_io.emit('clientEvent', res.data);
+      })
+      .catch(err => console.log(err));
+  }
 }
 
 module.exports = sensorData;
