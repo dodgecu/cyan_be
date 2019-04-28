e303be909d7dbc9b89ff3cf416aebcfb794b0e0e
Pavlo Pihula <rogertaylor1949@gmail.com>
2019-04-28 19:16:36 -0400

    Add submit flower

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -9,11 +9,7 @@ const User = require('../../models/user');
 
 //@route POST api/users
 router.post('/', (req, res) => {
-  const {
-    name,
-    email,
-    password
-  } = req.body;
+  const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({
@@ -21,51 +17,69 @@ router.post('/', (req, res) => {
     });
   }
 
-  User.findOne({ email })
-    .then(user => {
-      if (user) {
-        res.status(400).json({
-          message: 'User already exists'
-        });
-      }
-
-      const newUser = new User({
-        name,
-        email,
-        password
+  User.findOne({ email }).then(user => {
+    if (user) {
+      res.status(400).json({
+        message: 'User already exists'
       });
+    }
 
-      // Create salt & hash
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser.save()
-            .then(user => {
+    const newUser = new User({
+      name,
+      email,
+      password
+    });
 
-              jwt.sign(
-                { id: user.id },
-                config.get('JWT'),
-                { expiresIn: 3600 },
-                (err, token) => {
-                  if(err) throw err;
+    // Create salt & hash
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then(user => {
+          jwt.sign(
+            { id: user.id },
+            config.get('JWT'),
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
 
-                  res.json({
-                    token,
-                    user: {
-                      id: user.id,
-                      name: user.name,
-                      email: user.email
-                    }
-                  });
-
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
                 }
-              );
-
-            });
+              });
+            }
+          );
         });
       });
     });
+  });
+});
+
+//@route PUPT (update flower select) api/users
+router.put('/', (req, res) => {
+  const flowerId = { flowerId: req.body.flowerRecord };
+
+  if (!req.body) {
+    return res.status(400).send('Request body missing');
+  }
+  User.findOneAndUpdate(
+    {
+      _id: req.body.userRecord
+    },
+    {
+      $push: {
+        flowers: flowerId
+      }
+    }
+  )
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
